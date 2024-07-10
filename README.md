f02617322dfebe0d08c6e3bb71d3679d2a40cdb1
cperalt <145234956+cperalt@users.noreply.github.com>
2024-07-09 20:39:39 -0400

    Challenges

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-# React + Vite
+# React Guided Inquiry 1
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+My first React App
 
-Currently, two official plugins are available:
+The react application was initialized using Vit.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+The primary code for the challenge is located in the App.jsx file under src.
+
+For this guided inquiry the task was the following: 
+
+
+VERY EASY: Inside of your ‘App’ class ‘render’ method, return a div with your basic information for example name, number, date of birth and etc. This div should be hard-coded to prepare for the Medium Challenge.
+
+
+EASY: Create a ‘constructor’ that takes in ‘props’ , call the ‘super()’ method, and set ‘this.state’ to an empty object inside of the ‘App’ class. Inside of ‘this.state’ create a ‘person’ property and set it to an empty object
+
+
+
+MEDIUM: Replace all of the hard-coded with data from ‘this.state.person’ in the constructor.  Then take the returned ‘div’ and create a component, pass state into it and import it as ‘BasicInfo’. (This challenge should not change the look of the page)
+
+
+HARD: Now that you have a basic react app to display one person’s worth of information, now modify the app to iterate over an array of people’s basic information, while still keeping the new list of contacts in the top-level of state.
+(Optional: add styling to space out each person’s info)
+
+Thoughts: I would refactor my code to to make readability better. Currenlty all of the components are inside of the main App functional component as per the instructions. I would remove the class compenents each into their own separate file and import it into the main App.jsx file. In addition I would prefer to make the class components into functional components.
+
+> [!NOTE]
+> To run start the application type the following command `npm run dev`
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useState , Component } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +12,89 @@ function App() {
     'red',
     'blue',
     'green',
+  
   ]
+
+  const people = [
+    {
+        Name: "Queen",
+        Number: "453-267-4634",
+        DOB: "45-25-75",
+        Country: "Spain"
+    },
+    {
+        Name: "Rebeca",
+        Number: "232-343-6546",
+        DOB: "33-45-34",
+        Country: "France"
+
+    },
+    {
+        Name: "Jack",
+        Number: "765-324-6346",
+        DOB: "35-75-23",
+        Country: "United Kingdom"
+    },
+    {
+        Name: "Steph",
+        Number: "543-626-6456",
+        DOB: "35-35-86",
+        Country: "Poland"
+    },
+    {
+        Name: "Luke",
+        Number: "544-545-2323",
+        DOB: "32-54-23",
+        Country: "Pluto"
+    }
+    
+  ]
+
+
+  function BasicInfo({ person }) {
+    return (
+      <div className='p-card'>
+        <h2>Name: {person.Name}</h2>
+        <h2>Number: {person.Number}</h2>
+        <h2>Date of Birth: {person.DOB}</h2>
+        <h2>Country: {person.Country}</h2>
+      </div>
+    );
+  }
+
+  class Person extends Component { 
+    
+    constructor(props) { 
+    
+      // Calling super class constructor 
+      super(props); 
+        
+      // Creating state 
+      this.state = { 
+        person: {
+          Name: "Hughie",
+          Number: "287-343-5343",
+          DOB: "05-24-97",
+          Country: "United States"
+        },
+        people: people
+      } 
+        
+    } 
+    
+    render() { 
+      return ( 
+        <div className='container' > 
+          {/* <h2>Name: {this.state.person.Name}</h2>
+          <h2>Number: {this.state.person.Number}</h2>
+          <h2>DoB: {this.state.person.DOB}</h2>
+          <h2>Country: {this.state.person.Country}</h2> */}
+          <BasicInfo person={this.state.person}/>
+          {people.map(person => (<BasicInfo person={person} />))}
+        </div> 
+      ); 
+    } 
+  } 
 
   return (
     <>
@@ -37,6 +121,15 @@ function App() {
         <button style={{ backgroundColor: color, margin: 20}}>{`${color}`}</button>
         ) ) }
       </div>
+      <h1>Challenges:</h1>
+
+      {/* <div className="card">
+          <h2>Name: Hughie</h2>
+          <h2>Number: 287-343-5343</h2>
+          <h2>DoB: 05-24-97</h2>
+          <h2>Country: United States</h2>
+      </div> */}
+      <Person />
     </>
   )
 
